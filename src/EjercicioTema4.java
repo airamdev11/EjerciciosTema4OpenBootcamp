6f5b3c0453f9ea68bd6b07ebe2b64ce5e297a5a2
airamdev11 <airamdev11@outlook.com>
2022-08-13 01:21:35 -0500

    Se finalizan ejercicios

---
+++ src/EjercicioTema4.java
@@ -0,0 +1,55 @@
+public class EjercicioTema4 {
+
+
+
+
+
+
+    public static void main(String[] args) {
+        //Primera parte
+        int numeroIf = 4;
+        if(numeroIf<0){
+            System.out.println("El numero es negativo");
+        }
+        else if(numeroIf > 0){
+            System.out.println("El numero es positivo");
+        }
+        else{
+            System.out.println("El numero es un 0");
+        }
+
+        //Segunda parte
+        int numeroWhile = 0;
+        while(numeroWhile < 3){
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }
+
+        do{
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }while(numeroWhile<3);
+
+        //Tercera parte
+        for(int numeroFor = 0; numeroFor<=3; ++numeroFor){
+            System.out.println(numeroFor);
+        }
+
+        String estacion = "VERANO";
+
+        switch (estacion){
+            case "VERANO":System.out.println("Estas en la estacion de VERANO.");
+                break;
+            case "OTOÑO":System.out.println("Estas en la estacion de OTOÑO.");
+                break;
+            case "PRIMAVERA":System.out.println("Estas en la estacion de PRIMAVERA.");
+                break;
+            case "INVIERNO":System.out.println("Estas en la estacion de INVIERNO.");
+                break;
+            default:System.out.println("No se ingreso una estación valida.");
+                break;
+        }
+    }
+}
+
+
